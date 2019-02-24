bd31e4ffff6ee2001384f86d85ad0acaf335eaa0
Shuhratjan <nshuhr@gmail.com>
2019-02-24 15:18:24 +0600

    program is ready to read from Excel file and Show

--- Excel To Read From file/Form1.Designer.cs
+++ Excel To Read From file/Form1.Designer.cs
@@ -28,13 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cboSheet = new System.Windows.Forms.ComboBox();
+            this.labelSheet = new System.Windows.Forms.Label();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.btnOpen = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cboSheet
+            // 
+            this.cboSheet.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboSheet.FormattingEnabled = true;
+            this.cboSheet.Location = new System.Drawing.Point(353, 32);
+            this.cboSheet.Name = "cboSheet";
+            this.cboSheet.Size = new System.Drawing.Size(121, 21);
+            this.cboSheet.TabIndex = 0;
+            this.cboSheet.SelectedIndexChanged += new System.EventHandler(this.cboSheet_SelectedIndexChanged);
+            // 
+            // labelSheet
+            // 
+            this.labelSheet.AutoSize = true;
+            this.labelSheet.Location = new System.Drawing.Point(312, 35);
+            this.labelSheet.Name = "labelSheet";
+            this.labelSheet.Size = new System.Drawing.Size(38, 13);
+            this.labelSheet.TabIndex = 1;
+            this.labelSheet.Text = "Sheet:";
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(31, 59);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(745, 350);
+            this.dataGridView.TabIndex = 2;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(688, 415);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(88, 33);
+            this.btnOpen.TabIndex = 3;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 456);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.labelSheet);
+            this.Controls.Add(this.cboSheet);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cboSheet;
+        private System.Windows.Forms.Label labelSheet;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button btnOpen;
     }
 }
 
--- Excel To Read From file/Form1.cs
+++ Excel To Read From file/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Excel;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,31 @@ namespace Excel_To_Read_From_file
         public Form1()
         {
             InitializeComponent();
+        }
+        DataSet dataSetResult;
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog() { Filter = "Excel Workbook|*.xls", ValidateNames = true })
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    FileStream fileStream = File.Open(openFileDialog.FileName, FileMode.Open, FileAccess.Read);
+                    IExcelDataReader reader = ExcelReaderFactory.CreateBinaryReader(fileStream);
+                    reader.IsFirstRowAsColumnNames = true;
+                    dataSetResult = reader.AsDataSet();
+                    cboSheet.Items.Clear();
+                    foreach(DataTable dt in dataSetResult.Tables)
+                    {
+                        cboSheet.Items.Add(dt.TableName);
+                    }
+                    reader.Close();
+                }
+            }
+        }
+
+        private void cboSheet_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dataGridView.DataSource = dataSetResult.Tables[cboSheet.SelectedIndex];
         }
     }
 }
